c8b95f6520c732a9ad3c61148761439173dd1b16
jp.aria <jp.aria@JEAN-PIERRE-PC.Aplon.local>
2013-05-30 16:43:58 +0200

    Jquery suite

--- www/jquery/index.php
+++ www/jquery/index.php
@@ -1,23 +1,88 @@
 <?php 
 
-	function Get($Name, $Defaut){
-		if( isset($_GET[$Name]) ){
-			return trim($_GET[$Name]);
-		}
-		return $Defaut;
-	}
-	
-	function Post($Name, $Defaut){
-		if( isset($_POST[$Name]) ){
-			return trim($_POST[$Name]);
-		}
-		return $Defaut;
-	}
+	$Personnes = [];
 
+	Class Vars {
+		function get($Name, $Defaut) {
+			if( isset($_GET[$Name]) ){
+				return trim($_GET[$Name]);
+			}
+			return $Defaut;
+		}
+	}
+		
+	Class Personne {
 	
-	$Action = Get("action", "");
-	if($Action == "ok"){
-		echo "{\"text\": \"Resultat retourne par le serveur\", \"id\": \"10\"}";
+		var $nom = "";
+		var $prenom = "";
+		var $adresses = [];
+	}
+	
+	Class Adresse {
+		var $rue = "";
+		var $ville = "";
+		var $cp = "";
+	}
+	
+	$Vars = new Vars;
+	$Action = $Vars->get("action", "");
+	
+	if($Action == "obtenirPersonnes"){
+	
+		//Personne 1
+		//**********
+		
+		$Personne = new Personne;
+		$Personne->nom = "ARIA";
+		$Personne->prenom = "Jean-Pierre";
+		
+		$Adresse = new Adresse;
+		$Adresse->rue = "7 rue de l'église";
+		$Adresse->ville = "Berneville";
+		$Adresse->cp = "62123";
+		
+		array_push($Personne->adresses, $Adresse);
+		
+		$Adresse = new Adresse;
+		$Adresse->rue = "4 rue des pinsons";
+		$Adresse->ville = "Arras";
+		$Adresse->cp = "62000";	
+		
+		array_push($Personne->adresses, $Adresse);	
+	
+		array_push($Personnes, $Personne);
+		
+		//Personne 2
+		//**********
+		
+		$Personne = new Personne;
+		$Personne->nom = "ARIA";
+		$Personne->prenom = "Jean-Baptiste";
+		
+		$Adresse = new Adresse;
+		$Adresse->rue = "6 rue Emile Darras";
+		$Adresse->ville = "ACHICOURT";
+		$Adresse->cp = "62217";
+		
+		array_push($Personne->adresses, $Adresse);
+		
+		$Adresse = new Adresse;
+		$Adresse->rue = "4 rue des pinsons";
+		$Adresse->ville = "Arras";
+		$Adresse->cp = "62000";
+		
+		array_push($Personne->adresses, $Adresse);
+		
+		array_push($Personnes, $Personne);
+		
+		$json = Json_encode($Personnes);
+		
+		//$Personnes = Json_decode($json);	
+		//echo $Personnes[0]->nom . "<br />";		
+
+		echo $json;
+		exit();
+		
 	}
 	
 ?>
--- www/jquery/js/index.js
+++ www/jquery/js/index.js
@@ -1,3 +1,7 @@
+/*
+ * @jquery
+ */
+
 $(function(){
 	var t = "Longeur : " + $("div").length + "<br />" + "texte : " + $("div").text().trim() + "<br />" + "html : " +  $("div").html().trim();
 	$("div:eq(0)").html(t);
@@ -20,12 +24,12 @@ $(function(){
 		click : function(){
 			$(this).css("cursor", "default");
 			if($(this).width() == 250){
-				$(this).animate({width:'-=200', height:'-=200', borderRadius: "-=140", fontSize:"-=24", backgroundColor:"#df7708", opacity:1}, 1000 , function(){
+				$(this).animate({width:'-=200', height:"-=200", borderRadius: "-=140", fontSize:"-=24", backgroundColor:"#df7708", opacity:1}, 1000 , function(){
 					$(this).css({cursor: "pointer"}).text("un carré");
 				})
 			}
 			if($(this).width() == 50){
-				$(this).animate({width:'+=200', height:'+=200', borderRadius: "+=140", fontSize:"+=24", backgroundColor:"#0877df", opacity:0.5}, 1000, function(){
+				$(this).animate({width:'+=200', height:"+=200", borderRadius: "+=140", fontSize:"+=24", backgroundColor:"#0877df", opacity:0.5}, 1000, function(){
 					$(this).css({cursor: "pointer"}).text("un rond");					
 				})	
 			}
@@ -39,11 +43,11 @@ $(function(){
 	//$("span").attr("data-id", "test");
 	$("span").data("id", "test");
 	
-	$("#ajax").bind({
+	$("#ObtenirPersonne").bind({
 		
 		click: function(){
 			
-			$("#retour_ajax").html("Recherche en cours...");
+			$("#ResultatPersonne").html("Recherche des personnes en cours...");
 			
 			$.ajax({
 	            type: "GET",
@@ -51,12 +55,24 @@ $(function(){
 	            contentType: "application/json; charset=utf-8",
 	            dataType: "json",
 	            async: false,
-	            data: {"action":"ok"},
+	            data: {"action":"obtenirPersonnes"},
 	            error: function (xhr, status) {
 	            	alert("erreur Ajax!\n" + status);
 	            },
-	            success: function (data) {
-	            	$("#retour_ajax").html("id : " + data.id + "<br />texte : " + data.text);
+	            success: function (personnes) {
+	            	var html = "";
+	               	for( var i = 0; i < personnes.length; i++){
+		            	html += "<h2>Personne " + (i+1) + "</h2>";
+		            	html += "  Nom : " + personnes[i].nom;
+		            	html += "<br />  Prénom : " + personnes[i].prenom;		            	
+		            	for( var y = 0; y < personnes[i].adresses.length; y++){
+		            		html += "<br />    <b>Adresse " + (y+1) + "</b>";
+		            		html += "<br />      Rue : " + personnes[i].adresses[y].rue;
+		            		html += "<br />      Ville : " + personnes[i].adresses[y].ville;
+		            		html += "<br />      Code postal : " + personnes[i].adresses[y].cp;
+		            	}
+	               	}
+	            	$("#ResultatPersonne").html(html);
 
 	            }
 	        });
@@ -64,4 +80,244 @@ $(function(){
 		}
 	})
 	
+	$("#ModifierFormulaire").data("anim", false);
+		
+	$("#ModifierFormulaire").bind({
+		
+		click: function(){
+			var anim = $(this).data("anim")
+			if(anim){
+				return false;
+			}
+			$(this).data("anim", true);
+			var nom = $("input[name='nom']");
+			var prenom = $("input[name='prenom']");	
+			// 
+			if(nom.width() <= 200){
+				nom.val("ARIA");
+				prenom.val("Jean-Pierre");
+				//el.css({width:"500px", backgroundColor:"#fcc"});
+				nom.animate({width:"+=500px", backgroundColor:"#fcc"}, {duration: 1000, easing: "easeOutElastic", complete: function() {
+					$("#ModifierFormulaire").data("anim", false);
+			    }});
+				prenom.animate({width:"+=500px", backgroundColor:"#fcc"}, {duration: 1000, easing: "easeOutElastic"});				
+				$("form").animate({backgroundColor:"#ffc", border:"1px solid #611"});			
+			}
+			else{
+				nom.val("");
+				prenom.val("");
+				nom.animate({width:"-=500px", backgroundColor:"#fff"}, {duration: 1000, easing: "easeOutCirc", complete: function() {
+					$("#ModifierFormulaire").data("anim", false);					
+			    }});
+				prenom.animate({width:"-=500px", backgroundColor:"#fff"}, {duration: 1000, easing: "easeOutCirc"});				
+				$("form").animate({backgroundColor:"#fff", border:"1px solid #fff"});		
+			}
+			
+			return false;
+			
+		}
+	
+	});
+	
+	$("input[name='password']").bind({
+		keyup: function(){
+			if ($(this).next().get(0).tagName.toLowerCase() == "span"){
+				$(this).next().remove();
+			}			
+			var t = setPassword($(this).val());
+			$(this).after("<span class=\"" + t + "\">" + t + "</span>");
+			return false;
+		}
+	});
+	
+	$("input[name='passwordControl']").bind({
+		keyup: function(){
+			if ($(this).next().get(0).tagName.toLowerCase() == "img"){
+				$(this).next().remove();
+			}		
+			var vp = $("input[name='password']").val();
+			var vpc = $(this).val();
+			if(vp !== vpc){
+				$(this).after("<img src=\"../img/led_red_24.png\" alt=\"\" />");
+			}else{
+				$(this).after("<img src=\"../img/led_green_24.png\" alt=\"\" />");
+			}
+			return false;
+		}
+	});
+
+	
+	$("#demarrerChrono").bind({
+		click: function(){
+			Chrono.start("chrono");
+		}
+	});
+	
+	$("#arreterChrono").bind({
+		click: function(){
+			Chrono.stop();
+		}
+	});
+	
+	$("#effacerChrono").bind({
+		click: function(){
+			Chrono.raz();
+		}
+	});
+	
+	//Bouton Oui
+	$("#question button:eq(0)").bind({
+		mouseenter : function(){
+			var l1 = $(this).offset();			
+			var l2 = $("#question button:eq(0)").offset();
+			$(this).offset({left: l2.left});
+			$("#question button:eq(0)").offset({left: l1.left});
+			
+		},
+		
+		click : function(){
+			$("#question h3:eq(0)").html("C'est bien ce que nous pensions");
+		}
+		
+	});
+	
+	//Bouton Non
+	$("#question button:eq(1)").bind({
+		mouseenter : function(){
+			var l1 = $("#question button:eq(0)").offset();
+			var l2 = $(this).offset();
+			$(this).offset({left: l1.left});
+			$("#question button:eq(0)").offset({left: l2.left});;			
+		},
+	
+		click : function(){
+			$("#question h3:eq(0)").html("C'est dommage");
+		}
+	});
+	
+	//image sur souris	
+	$("body").append($("<div id='souris' />"));	
+	$(document).mousemove(function(e){		 
+	      $('#souris').show().offset({left: (e.pageX + 5), top: (e.pageY + 5)});
+	});
+	$(document).mouseout(function(e){		 
+	      $('#souris').hide();
+	});
+	
+	//images survolées
+	var div = $("<div />")
+	$("body").append(div);
+	div.css({float: "left"});
+	div.append($("<img src='../img/img1.png' alt='' />"));
+	div.append($("<img src='../img/img2.png' alt='' />"));
+	div.append($("<img src='../img/img3.png' alt='' />"));
+	div.append($("<img src='../img/img4.png' alt='' />"));
+	
+	div.children().each(function(){
+		$(this).mouseover(function(){
+			$(this).animate({width : "+= 50", height: "+=50"}, {duration: 500, easing: "easeOutElastic"});
+		});
+		$(this).mouseout(function(){
+			$(this).animate({width : "-= 50", height: "-=50"}, {duration: 500, easing: "easeOutElastic"});
+		});
+	});
+	 
+	//Volet mobile
+	Volet.init();
+		
+	
 });
+
+
+var Volet = new function(){
+	
+	this.init = function(){
+		var v = $("<div class='volet' />"); 
+		for (var i = 0; i < 10; i++){
+			var b = $("<div />");
+			b.html("Menu " + (i + 1));
+			v.append(b);
+			b.click(function(){
+				alert($(this).text())
+			});
+		}
+		var s =  $("<span />");
+		v.append(s);
+		s.click(function(){
+			if(s.parent().offset().left < -200){
+				s.parent().animate({left: "+=190"}, {duration: 1000, easing: "easeOutElastic"});
+			}else{
+				s.parent().animate({left: "-=190"}, {duration: 1000, easing: "easeOutElastic"});
+			}
+		});
+		$("body").append(v);
+		v.css({height: $(window).height()});
+	};
+}
+
+$(window).scroll(function(){
+	$(".volet").offset({top: this.scrollY})
+})
+
+var Chrono = new function(){
+	
+	_this = this;
+	this.timer = undefined;	
+	this.el = undefined;
+	this.time = 0;
+	
+	this.convert = function(i){		
+		var secondes = i;
+		var d = new Date();
+		d.setTime(this.time * 10);
+		var h = ((d.getHours()-1) >= 10) ? (d.getHours()-1) : "0" + (d.getHours()-1);
+		var m = ((d.getMinutes()) >= 10) ? (d.getMinutes()) : "0" + (d.getMinutes());
+		var s = ((d.getSeconds()) >= 10) ? (d.getSeconds()) : "0" + (d.getSeconds());
+		var ms = ((d.getMilliseconds()) >= 10) ? (d.getMilliseconds().toString().substr(0, 2)) : "0" + (d.getMilliseconds().toString().substr(0, 2));
+		$("#chrono").html(h + ":" + m + ":" + s + "." + ms);
+	}
+	
+	this.increment = function(){
+		_this.time++;
+		_this.convert(_this.time);
+	}
+	
+	this.start = function(id){
+		_this.el = $("#"+ id);
+		_this.timer = window.setInterval(_this.increment, 10);
+	};
+	
+	this.stop = function(){
+		window.clearInterval(_this.timer);
+		_this.timer = undefined;
+	};
+	
+	this.raz = function(){
+		_this.stop();
+		_this.time = 0;
+		this.convert(_this.time);
+	};
+}
+
+function setPassword(mdp){
+	var sFort = "[A-Z]" + //commence par une majuscule
+		"(?=(.*[a-z]){2,})" + //contient au moins deux minuscule
+		"(?=(.*[0-9]){2,})" , //contient au moins deux chiffres
+
+	sMoyen ="(?=(.*[a-z]){2,})" + //contient au moins deux minuscule
+		"(?=(.*[0-9]){2,})", //contient au moins deux chiffres
+		
+	regFort = new RegExp(sFort),
+	regMoyen = new RegExp(sMoyen);
+
+	if ((mdp.match(regFort)) && (mdp.length>=8)) {
+		return "fort";
+	}
+
+	if ((mdp.match(regMoyen)) && (mdp.length>=8)) {
+		return "moyen";
+	}
+
+	return "faible";
+
+}
